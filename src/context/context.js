31578e5553807cf31ae06d8287c8d652e5e535b0
GAGAN2608 <46592307+GAGAN2608@users.noreply.github.com>
2020-02-01 22:48:57 +0530

    Contentful Data, Env Variables

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { linkData } from "./linkData";
 import { socialData } from "./socialData";
-import { items } from "./productData";
+//import { items } from "./productData";
+import { client } from "./contentful";
 const ProductContext = React.createContext();
 //Provider
 //Consumer
@@ -31,7 +32,15 @@ class ProductProvider extends Component {
   componentDidMount() {
     //from contentful items
 
-    this.setProducts(items);
+    //this.setProducts(items);
+    //l
+    client
+      .getEntries({
+        content_type: "techStoreProducts"
+      })
+      .then(response => this.setProducts(response.items))
+      .catch(console.error);
+
   }
 
   //set products
